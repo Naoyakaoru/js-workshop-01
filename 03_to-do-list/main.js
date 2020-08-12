b6b71eb71da0c84f4a1ee18f354c1376003dd1ee
Kelsey.Chen <naoyakaoru@gmail.com>
2020-08-12 14:49:25 +0800

    start fix drag

--- 03_to-do-list/main.js
+++ 03_to-do-list/main.js
@@ -1,24 +1,55 @@
 document.addEventListener('DOMContentLoaded', function() {
   const ul = document.querySelector('ul')
   const eachLi = document.querySelectorAll('li')
-  const eachClose = document.querySelectorAll('.close')
+  const eachCloseBtn = document.querySelectorAll('.close')
   const input = document.querySelector('#input')
   const addBtn = document.querySelector('#addBtn')
-  
+  var drag = {
+    current: null,
+    yValue: 0,
+    place: 0
+  }
+
   for(i = 0 ; i < eachLi.length ; i++){
     eachLi[i].addEventListener('click', function(){
     this.classList.toggle('checked')
   })
   }
   
-  eachClose.forEach(btn => {
+  eachCloseBtn.forEach(btn => {
     btn.addEventListener('click', function(){
       this.parentNode.remove(btn.parentNode)
     })
   });
   
+  eachLi.forEach(li => {
+    li.addEventListener('dragstart', function(drag){
+      drag.current = drag.target
+      
+      
+      console.log(this.offsetTop)
+      console.log(e.pageY)
+      console.log(this.clientHeight)
+    })
+
+    li.addEventListener('drag', function(e) {
+      // this.hidden = true
+      console.log(ul.offsetTop)
+      console.log(e.pageY)
+    })
+
+    li.addEventListener('dragend', function(e){
+      console.log('dragend')
+      // ul.removeChild(this)
+      let place = Math.floor((e.pageY - ul.offsetTop) / 40)
+      console.log(place)
+      
+    })
+  });
+
   addBtn.addEventListener('click', function(){
     let newLi = document.createElement('li')
+    newLi.setAttribute('draggable', 'true')
     newLi.innerHTML = `${input.value}<span class="close">x</span>`
     ul.appendChild(newLi)
     input.value = ''
