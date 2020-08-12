8572c287f93222fc8e059f7031d752fe0dc6c017
Kelsey.Chen <naoyakaoru@gmail.com>
2020-08-12 11:34:33 +0800

    checked function finished

--- 03_to-do-list/main.js
+++ 03_to-do-list/main.js
@@ -1,1 +1,11 @@
-// TO DO
+document.addEventListener('DOMContentLoaded', function() {
+  const eachLi = document.querySelectorAll('li')
+  
+  for(i = 0 ; i < eachLi.length ; i++){
+    eachLi[i].addEventListener('click', function(){
+    this.classList.toggle('checked')
+  })
+  }
+  
+  
+})
