3f2ff138fdd6b5918ad8eb69fc4b8eb4579325c3
Kelsey.Chen <naoyakaoru@gmail.com>
2020-08-12 12:18:02 +0800

    finished todolist 01-03

--- 03_to-do-list/main.js
+++ 03_to-do-list/main.js
@@ -1,5 +1,9 @@
 document.addEventListener('DOMContentLoaded', function() {
+  const ul = document.querySelector('ul')
   const eachLi = document.querySelectorAll('li')
+  const eachClose = document.querySelectorAll('.close')
+  const input = document.querySelector('#input')
+  const addBtn = document.querySelector('#addBtn')
   
   for(i = 0 ; i < eachLi.length ; i++){
     eachLi[i].addEventListener('click', function(){
@@ -7,5 +11,27 @@ document.addEventListener('DOMContentLoaded', function() {
   })
   }
   
+  eachClose.forEach(btn => {
+    btn.addEventListener('click', function(){
+      this.parentNode.remove(btn.parentNode)
+    })
+  });
   
+  addBtn.addEventListener('click', function(){
+    let newLi = document.createElement('li')
+    newLi.innerHTML = `${input.value}<span class="close">x</span>`
+    ul.appendChild(newLi)
+    input.value = ''
+    
+    let newClose = newLi.querySelector('.close')
+    newLi.addEventListener('click', function(){
+      this.classList.toggle('checked')
+    })
+    
+    newClose.addEventListener('click', function(){
+      this.parentNode.remove(newClose.parentNode)
+    })
+    
+  })  
+
 })
